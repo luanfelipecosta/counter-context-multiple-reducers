3f0f35a5279c41efb86d4eed933359577a9a62bb
Costa <luan.costa@devires.com.br>
2020-05-27 18:42:18 -0300

    button-order-changed

--- src/counter/counter.component.js
+++ src/counter/counter.component.js
@@ -4,9 +4,9 @@ import React from 'react';
 const Counter = ({ count, increment, decrement }) => {
   return (
     <div>
-      <button onClick={increment}>+</button>
-      <input disabled value={count} />
       <button onClick={decrement}>-</button>
+      <input disabled value={count} />
+      <button onClick={increment}>+</button>
     </div>
   );
 };
